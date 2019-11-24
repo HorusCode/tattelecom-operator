ad59dbc83bce6ea1b885c50b70dceacc5c870b6c
Horus Dev <horusm99@gmail.com>
2019-11-24 20:54:42 +0300

    Install npm packages, install ide-helper,init auth

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'Auth\LoginController@showLoginForm')->name('login');
+
+Route::get('/home', 'HomeController@index')->name('home');
