0d2ea542f9ba333e7c54bc7e9165a913c5e9378d
Horus Dev <horusm99@gmail.com>
2019-11-25 21:19:48 +0300

    Disable bootstrap, install normalize.css

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -15,5 +15,9 @@ return [
 
     'failed' => 'These credentials do not match our records.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-
+    'login' => 'Login',
+    'email' => 'Email',
+    'forgot' => 'Forgot Your Password?',
+    'password' => 'Password',
+    'remember' => 'Remember me'
 ];
