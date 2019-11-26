718bf51566be631697e56169d40489bcf1bd2957
Horus Dev <horusm99@gmail.com>
2019-11-26 17:56:48 +0300

    Create simple login page, install mdi font icon

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,14 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid p-0">
-    <div class="row justify-content-center">
-        <div class="col-md-7">
-            <img src="{{ asset('image/auth_bg.jpg') }}" class="bg--image" alt="Bg">
-        </div>
-        <div class="col-md-5">
-
+    <div class="grid-login">
+        <div class="col-bg" style="background-image: url('{{ asset('image/auth_bg.jpg') }}')"></div>
+        <div class="col-form flex-col justify-content-center align-items-center">
+            <h1>Логин</h1>
+            <form class="form">
+                <div class="input-line">
+                    <label class="input-label" for="login">{{ __('auth.login') }}</label>
+                    <input class="input" type="text" id="login" name="login">
+                </div>
+                <div class="input-line">
+                    <label class="input-label" for="password">{{__('auth.password')}}</label>
+                    <input class="input" type="text" id="password" name="password">
+                </div>
+                <div class="form--button-group">
+                    <button type="button" class="btn btn-primary">Войти</button>
+                </div>
+            </form>
         </div>
     </div>
-</div>
 @endsection
